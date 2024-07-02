29e01404c24c69e8742e21a96237c07dec67c82f
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-07-02 12:32:44 +0200

    feat: migrated to generic v6.0.0

--- src/main/java/ch/sbb/polarion/extension/api/extender/rest/ApiExtenderRestApplication.java
+++ src/main/java/ch/sbb/polarion/extension/api/extender/rest/ApiExtenderRestApplication.java
@@ -7,34 +7,30 @@ import ch.sbb.polarion.extension.api.extender.rest.controller.ProjectCustomField
 import ch.sbb.polarion.extension.api.extender.settings.GlobalRecordsSettings;
 import ch.sbb.polarion.extension.api.extender.settings.ProjectCustomFieldsSettings;
 import ch.sbb.polarion.extension.generic.rest.GenericRestApplication;
-import ch.sbb.polarion.extension.generic.settings.GenericNamedSettings;
 import ch.sbb.polarion.extension.generic.settings.NamedSettingsRegistry;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Set;
 
 public class ApiExtenderRestApplication extends GenericRestApplication {
 
     public ApiExtenderRestApplication() {
-        List<GenericNamedSettings<?>> settingsList = new ArrayList<>();
-        settingsList.add(new GlobalRecordsSettings());
-        settingsList.add(new ProjectCustomFieldsSettings());
-
-        NamedSettingsRegistry.INSTANCE.register(settingsList);
+        NamedSettingsRegistry.INSTANCE.register(
+                Arrays.asList(
+                        new GlobalRecordsSettings(),
+                        new ProjectCustomFieldsSettings()
+                )
+        );
     }
 
     @Override
-    @NotNull
-    protected Set<Class<?>> getControllerClasses() {
-        final Set<Class<?>> controllerClasses = super.getControllerClasses();
-        controllerClasses.addAll(Set.of(
-                GlobalRecordApiController.class,
-                GlobalRecordInternalController.class,
-                ProjectCustomFieldApiController.class,
-                ProjectCustomFieldInternalController.class
-        ));
-        return controllerClasses;
+    protected @NotNull Set<Object> getExtensionControllerSingletons() {
+        return Set.of(
+                new GlobalRecordApiController(),
+                new GlobalRecordInternalController(),
+                new ProjectCustomFieldApiController(),
+                new ProjectCustomFieldInternalController()
+        );
     }
 }
