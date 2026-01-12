b60b29242ed3e73e5aa854d68070517805c70594
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-01-12 13:45:20 +0100

    fix: rename `setField` to `setRecord` in `VelocityGlobalRecords`

--- src/main/java/ch/sbb/polarion/extension/api_extender/velocity/VelocityGlobalRecords.java
+++ src/main/java/ch/sbb/polarion/extension/api_extender/velocity/VelocityGlobalRecords.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @NoArgsConstructor
 public class VelocityGlobalRecords extends VelocityReadOnlyGlobalRecords {
 
-    public void setField(@NotNull String key, @NotNull String value) throws JAXBException, IOException {
+    public void setRecord(@NotNull String key, @NotNull String value) throws JAXBException, IOException {
         new GlobalRecords().setRecord(key, value);
     }
 }
